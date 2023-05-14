1bab04a9be7e9a85d9e7460f5baeed90fd4bdd52
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 15:28:28 +0900

    feat(auth): add login endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth API')
 @Controller()
-export class AppController {}
+export class AppController {
+  @UseGuards(AuthGuard('local'))
+  @HttpCode(HttpStatus.OK)
+  @ApiConsumes('application/json')
+  @ApiBody({
+    schema: {
+      properties: {
+        username: { type: 'string' },
+        password: { type: 'string' },
+      },
+    },
+  })
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
