76bd1e612669fa7339c128fe7ce2c0492ce236d5
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 12:17:58 +0900

    chore(db): add database related configs and deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "poapper_hackathon",
+      entities: [],
+      synchronize: true
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
