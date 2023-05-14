c08dbfc5de601622c9f1355cc62fc51139bfb6ad
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 14:19:12 +0900

    feat(user): add `UserService` to exported service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,6 +6,7 @@ import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  exports: [UserService],
   controllers: [UserController],
   providers: [UserService],
 })
