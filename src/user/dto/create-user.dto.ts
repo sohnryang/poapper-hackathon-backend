10b4be4e3a7ae7ea9bea66661ee065b0854b0d02
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 13:10:10 +0900

    feat(user): add DTOs for user endpoint

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ description: 'POVIS username of user' })
+  username: string;
+
+  @ApiProperty({ description: 'POSTECH student ID' })
+  studentId: string;
+
+  @ApiProperty({ description: 'Full name of user' })
+  fullName: string;
+
+  @ApiProperty({ description: 'Cell phone number of user' })
+  phoneNumber: string;
+
+  @ApiProperty({ description: 'Password for user' })
+  password: string;
+}
