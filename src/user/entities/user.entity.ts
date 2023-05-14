a3c93f3fe5d31780ad0fdca77c3852841678cca5
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 13:09:53 +0900

    feat(user): add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  studentId: string;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  @Exclude()
+  passwordHash: string;
+}
