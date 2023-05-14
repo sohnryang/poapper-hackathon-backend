a0915ba729131896e587ffc8ef85c01a93b15075
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 16:32:47 +0900

    docs(swagger): annotate auth API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,14 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
@@ -15,18 +22,39 @@ import { LocalAuthGuard } from './auth/local-auth.guard';
 export class AppController {
   constructor(private authService: AuthService) {}
 
+  @Post('auth/login')
   @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
   @ApiConsumes('application/json')
+  @ApiOperation({ summary: 'Login and get token' })
   @ApiBody({
     schema: {
       properties: {
-        username: { type: 'string' },
-        password: { type: 'string' },
+        username: {
+          type: 'string',
+          description: 'POVIS username',
+          example: 'povisusername',
+        },
+        password: {
+          type: 'string',
+          description: 'Password',
+          example: 'weakpassword',
+        },
       },
     },
   })
-  @Post('auth/login')
+  @ApiOkResponse({
+    description: 'Login succeeded.',
+    schema: {
+      properties: {
+        token: {
+          type: 'string',
+          description: 'JWT token',
+        },
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({ description: 'Invalid username or password.' })
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
