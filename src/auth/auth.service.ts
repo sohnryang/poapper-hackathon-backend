81a8b5b5c41615719054520aeaed12fe600398d6
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 14:28:55 +0900

    feat(auth): add user validation and local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as argon2 from 'argon2';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.userService.findOneByUsername(username);
+    if (user && (await argon2.verify(user.passwordHash, password)))
+      return { id: user.id };
+    return null;
+  }
+}
