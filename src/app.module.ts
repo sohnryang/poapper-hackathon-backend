36210d76bf2dbba1146153c7c0ed0161c8bb3782
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 13:08:37 +0900

    feat(app): add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
-import {Module} from '@nestjs/common';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {UserModule} from './user/user.module';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
+      type: 'mysql',
+      host: 'localhost',
       port: 3306,
-      username: "root",
-      password: "",
-      database: "poapper_hackathon",
-      entities: [],
-      synchronize: true
+      username: 'root',
+      password: '',
+      database: 'poapper_hackathon',
+      entities: [User],
+      synchronize: true,
     }),
-    UserModule
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
