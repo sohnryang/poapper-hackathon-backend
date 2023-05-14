41aaaecbc9294ad74c24e1c40628c0ab6aa139d3
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 16:11:13 +0900

    docs(swagger): add swagger decorators to `User`

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,21 +1,27 @@
+import { ApiResponseProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
+  @ApiResponseProperty({ example: 42 })
   id: number;
 
   @Column()
+  @ApiResponseProperty({ example: 'povisusername' })
   username: string;
 
   @Column()
+  @ApiResponseProperty({ example: '20220123' })
   studentId: string;
 
   @Column()
+  @ApiResponseProperty({ example: 'Nix Po' })
   fullName: string;
 
   @Column()
+  @ApiResponseProperty({ example: '01012345678' })
   phoneNumber: string;
 
   @Column()
