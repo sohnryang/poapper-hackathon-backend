ef5be41b001d29cafebe523a1ef20fada106fd60
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 16:19:41 +0900

    docs(swagger): add swagger docs to user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,8 +16,17 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { User } from './entities/user.entity';
 
 @ApiTags('User API')
 @Controller('user')
@@ -32,21 +41,28 @@ export class UserController {
 
   @Get()
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOkResponse({ description: 'Query succeeded.', type: User, isArray: true })
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOkResponse({ description: 'Query succeeded.', type: User })
+  @ApiNotFoundResponse({ description: 'User with ID not found.' })
   async findOne(@Param('id') id: string) {
     const user = await this.userService.findOne(+id);
     if (!user) throw new NotFoundException();
     return user;
   }
 
+  @Patch(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @Patch(':id')
+  @ApiOkResponse({ description: 'Edited successfully.' })
+  @ApiNotFoundResponse({ description: 'User wit ID not found.' })
+  @ApiForbiddenResponse({ description: 'Editing other user.' })
+  @ApiUnauthorizedResponse({ description: 'Invalid token.' })
   async update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -58,9 +74,13 @@ export class UserController {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @Delete(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @Delete(':id')
+  @ApiOkResponse({ description: 'Deleted successfully.' })
+  @ApiNotFoundResponse({ description: 'User wit ID not found.' })
+  @ApiForbiddenResponse({ description: 'Deleting other user.' })
+  @ApiUnauthorizedResponse({ description: 'Invalid token.' })
   async remove(@Param('id') id: string, @Request() req) {
     const user = await this.userService.findOne(+id);
     if (!user) throw new NotFoundException();
