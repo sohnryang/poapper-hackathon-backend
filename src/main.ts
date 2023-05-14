4f50a6819351d5857dbdb83001e92e22fab7015c
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 13:09:25 +0900

    docs(swagger): add swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('PoApper Hackathon Backend')
+    .setVersion('0.0')
+    .build();
+  const apiDocs = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, apiDocs);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
