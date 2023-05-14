c57c3bd868f0b1acebb39478b7372794aa72ba6e
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 17:28:29 +0900

    feat(app): switch to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,8 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'poapper_hackathon',
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
       entities: [User],
       synchronize: true,
     }),
