b1b4acf92fd2da88a2e9816c2e6ee363c38fc402
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 18:25:06 +0900

    feat(post): generate post endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,20 @@ import { AppService } from './app.service';
 import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+import { Post } from './post/entities/post.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [User],
+      entities: [User, Post],
       synchronize: true,
     }),
     UserModule,
     AuthModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
