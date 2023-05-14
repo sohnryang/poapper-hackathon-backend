5612c543b5f494ff888da2a9f9a68e198628e5dd
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-14 18:31:32 +0900

    feat(post): add post DTO

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePostDto {
+  @ApiProperty({ description: 'Title of post' })
+  title: string;
+
+  @ApiProperty({ description: 'Description in post' })
+  description: string;
+
+  @ApiProperty({ description: 'Link to image' })
+  imageLink: string;
+
+  @ApiProperty({ description: 'Registration deadline' })
+  registerationDeadline: string;
+}
